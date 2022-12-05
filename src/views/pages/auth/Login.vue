e63544017e3c8c8a6571ed2ccc671ba297b629f1
lapinRagnar <lapinragnar@gmail.com>
2022-12-05 23:18:15 +0100

    10 - login

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,9 +1,124 @@
 <template>
   <div>
-    <h1>Login</h1>
+  <v-row
+    class="background conteneur"
+  >
+      <v-col cols="10" lg="4" >
+        <v-card
+          class=" text-center pa-4 bg-indigo-lighten-4"
+          max-width="600"
+          variant="flat"
+        >
+
+        <v-avatar color="surface-variant" size="100">
+          <v-icon size="60" icon="mdi:mdi-account"></v-icon>
+        </v-avatar>
+
+
+
+
+        <v-form
+          ref="form"
+          v-model="state.isValid"
+
+        >
+
+          <v-card-text>
+
+            <v-text-field
+              v-model="state.email"
+              :rules="state.emailRules"
+              label="E-mail"
+              required
+              type="email"
+              prepend-inner-icon="mdi:mdi-account"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="state.password"
+              :rules="state.passwordRules"
+              label="Password"
+              required
+              :type="state.passwordShow ? 'text' : 'password'"
+              prepend-inner-icon="mdi:mdi-key"
+              :append-inner-icon="state.passwordShow ? 'mdi:mdi-eye' : 'mdi:mdi-eye-off'"
+              @click:append-inner="(state.passwordShow = !state.passwordShow)"
+            ></v-text-field>
+
+            <v-switch></v-switch>
+
+          </v-card-text>
+
+          <v-card-actions>
+
+            <v-btn
+              color="success"
+              class="mr-4"
+              @click="valider"
+            >
+              Login
+            </v-btn>
+
+          </v-card-actions>
+
+        </v-form>
+
+
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script setup>
 
+  import { reactive, ref } from 'vue'
+
+  const form = ref(null)
+
+  const state = reactive({
+    isValid: false,
+    password: '',
+    passwordShow: false,
+    email: '',
+    passwordRules: [
+      v => !!v || 'Name is required',
+      v => v.length >= 4 || 'Password must 4 characters and more',
+    ],
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+/.test(v) || 'E-mail must be valid',
+    ],
+  })
+
+  async function valider(){
+    const { valid } = await form.value.validate()
+    console.log('valid', valid)
+    console.log('utilisateur connecté!');
+  }
+
+
+
 </script>
+
+
+<style lang="scss">
+  .background {
+    background-image: url('yellow-green-abstract.jpg');
+    max-height: 600px;
+    width: 100%;
+    display: block;
+    position: absolute;
+    top: 0;
+    background-size: cover;
+  }
+
+  .conteneur {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+</style>
